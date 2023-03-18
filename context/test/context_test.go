7605c4bac6009aed58b6c4c1fbcdb6991612f88b
Vinicius <vfranca477@gmail.com>
2023-03-18 07:17:15 -0300

    refazendo pra entender

--- context/test/context_test.go
+++ context/test/context_test.go
@@ -3,38 +3,85 @@ package test
 import (
 	"context"
 	"context-tdd/entity"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 )
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("não implementado")
+}
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
+type SpyStore struct {
+	response string
+	t        testing.T
+}
+
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store foi cancelado")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
+}
+
 func TestHandler(t *testing.T) {
 	data := "olá, mundo"
-	t.Run("avisa a store para cancelar o trabalho se a requisicao for cancelada", func(t *testing.T) {
-		store := &entity.SpyStore{Response: data, T: t}
-		svr :=
-			entity.Server(store)
+	t.Run("avisa a store para cancelar o trabalho se a requisição for cancelada", func(t *testing.T) {
+		store := &SpyStore{response: data}
+
+		svr := entity.Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
 		cancellingCtx, cancel := context.WithCancel(request.Context())
 		time.AfterFunc(5*time.Millisecond, cancel)
-
-		//estou adicionando o contexto a minha solicitacao
 		request = request.WithContext(cancellingCtx)
-		response := &entity.SpyResponseWriter{}
+
+		response := &SpyResponseWriter{}
 
 		svr.ServeHTTP(response, request)
 
-		if response.Written {
+		if response.written {
 			t.Error("uma resposta não deveria ter sido escrita")
 		}
 
 	})
 
 	t.Run("retorna dados da store", func(t *testing.T) {
-		store := entity.SpyStore{Response: data, T: t}
+		store := SpyStore{response: data}
 		svr := entity.Server(&store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
