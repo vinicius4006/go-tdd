2ccfb172190692fbcb7c0f5bb9b843c97110a18a
Vinicius <vfranca477@gmail.com>
2023-03-24 23:34:26 -0300

    correcao

--- aplicacao-http/main.go
+++ aplicacao-http/main.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 
-	server := &ServidorJogador{Armazenamento: NovoArmazenamentoJogadorEmMemoria()}
+	server := NovoServidorJogador(NovoArmazenamentoJogadorEmMemoria())
+	//server := &ServidorJogador{Armazenamento: NovoArmazenamentoJogadorEmMemoria()}
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("não foi possível escutar na porta 5000 %v", err)
 	}
